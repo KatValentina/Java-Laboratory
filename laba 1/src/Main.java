86aebc7aeea6381e37969e4eb2cc6a41b24776fd
Valentina <v4lentinakataeva@yandex.ru>
2025-09-14 17:39:12 +0500

    Сохранение задания 2 и 3

--- laba 1/src/Main.java
+++ laba 1/src/Main.java
@@ -1,33 +1,33 @@
 import java.util.Scanner;
+import java.util.Random;
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int number;
         System.out.print("Номер задания: ");
         Scanner scanner = new Scanner(System.in);
         number= scanner.nextInt();
-        switch(number){
-            case 1:
-            {
+        switch (number) {
+            case 1: {
                 int num;
                 System.out.print("Номер подзадания: ");
                 num= scanner.nextInt();
-                switch(num){
-                    case 2:{
+                switch (num){
+                    case 2: {
                         int x;
                         System.out.print("Введите число x: ");
                         x = scanner.nextInt();
-                        if(x>=10){
+                        if (x>=10) {
                             Metod t = new Metod();
                             int sum;
                             sum = t.sumLastNums (x);
                             System.out.print("Сумма двух последних цифр числа x: "+sum);
-                        }
-                        else{
+                        } else {
                             System.out.println("Число x должно быть не менее 10");
                         }
                         break;
                     }
-                    case 4:{
+                    case 4: {
                         int x;
                         System.out.print("Введите число x: ");
                         x = scanner.nextInt();
@@ -41,9 +41,8 @@ public class Main {
                     }
                     case 6: {
                         char x;
-                        scanner.nextLine();// "очищаем буфер", т.к при прошлом вызове scanner.nextInt() был перенос строки
                         System.out.print("Введите символ x: ");
-                        x = scanner.nextLine().charAt(0); // считываем строку и считываем 1 символ
+                        x = scanner.next().charAt(0);
                         Metod t = new Metod();
                         boolean answer;
                         answer = t.isUpperCase (x);
@@ -52,7 +51,7 @@ public class Main {
                         System.out.println(answer);
                         break;
                     }
-                    case 8:{
+                    case 8: {
                         int a,b;
                         System.out.print("Введите число a: ");
                         a = scanner.nextInt();
@@ -67,7 +66,7 @@ public class Main {
                         System.out.println(answer);
                         break;
                     }
-                    case 10:{
+                    case 10: {
                         int a,b;
                         System.out.print("Введите число a: ");
                         a = scanner.nextInt();
@@ -75,7 +74,7 @@ public class Main {
                         b = scanner.nextInt();
                         Metod t = new Metod();
                         a=t.lastNumSum(a,b);
-                        for(int i=0;i<3;i++){
+                        for (int i=0;i<3;i++) {
                             System.out.println("Число а(сумма последних цифр 2 чисел): "+a);
                             System.out.print("Введите число b: ");
                             b = scanner.nextInt();
@@ -87,13 +86,140 @@ public class Main {
                     default: System.out.println("Нет такого подзадания"); break;
                 }
             }
-            case 2:
-            {
+            case 2: {
+                int num;
+                System.out.print("Номер подзадания: ");
+                num= scanner.nextInt();
+                switch (num) {
+                    case 2: {
+                        int x,y;
+                        System.out.print("Введите число x: ");
+                        x = scanner.nextInt();
+                        System.out.print("Введите число y: ");
+                        y = scanner.nextInt();
+                        Metod t = new Metod();
+                        double answer;
+                        answer = t.safeDiv(x,y);
+                        System.out.println("Результат деления x на y: "+answer);
+                        break;
+                    }
+                    case 4: {
+                        int x,y;
+                        System.out.print("Введите число x: ");
+                        x = scanner.nextInt();
+                        System.out.print("Введите число y: ");
+                        y = scanner.nextInt();
+                        Metod t = new Metod();
+                        String answer;
+                        answer = t.makeDecision(x,y);
+                        System.out.println("Результат сравнения x и y: "+answer);
+                        break;
+                    }
+                    case 6: {
+                        int x,y,z;
+                        System.out.print("Введите число x: ");
+                        x = scanner.nextInt();
+                        System.out.print("Введите число y: ");
+                        y = scanner.nextInt();
+                        System.out.print("Введите число z: ");
+                        z = scanner.nextInt();
+                        Metod t = new Metod();
+                        boolean answer;
+                        answer = t.sum3(x,y,z);
+                        System.out.println("Можно ли два любых числа из трех принятых сложить так, \n"+
+                                        "чтобы получить третье? : "+answer);
+                        break;
+                    }
+                    case 8: {
+                        int x;
+                        System.out.print("Введите число x: ");
+                        x = scanner.nextInt();
+                        if (x<=0) {
+                            System.out.println("Нет отрицательного и равного 0 кол-ва лет");
+                        } else {
+                            Metod t = new Metod();
+                            String answer;
+                            answer = t.age(x);
+                            System.out.println(answer);
+                        }
+                        break;
+                    }
+                    case 10: {
+                        String weekday;
+                        System.out.print("Введите день недели с маленькой буквы, " +
+                                "иначе он не будет считан: ");
+                        weekday = scanner.next(); // использывала только next(), т.к он дожидается ввода кокого-либо
+                        Metod t = new Metod(); // символа, а также не считывает остальные символы после пробела
+                        System.out.println("Дни недели: ");
+                        t.printDays(weekday);
+                        break;
+                    }
+                    default: System.out.println("Нет такого подзадания"); break;
+                }
                 break;
             }
-            case 3:
-            {
-
+            case 3: {
+                int num;
+                System.out.print("Номер подзадания: ");
+                num = scanner.nextInt();
+                switch (num) {
+                    case 2: {
+                        int x;
+                        System.out.print("Введите число: ");
+                        x = scanner.nextInt();
+                        if (x<0) {
+                            System.out.print("Неправильный ввод!");
+                        } else {
+                            Metod t = new Metod();
+                            String answer = t.reverseListNums(x);
+                            System.out.print("Обратный отсчёт: "+answer);
+                        }
+                        break;
+                    }
+                    case 4: {
+                        int x,y;
+                        System.out.print("Введите число х: ");
+                        x = scanner.nextInt();
+                        System.out.print("Введите степень числа x (только положительную): ");
+                        y = scanner.nextInt();
+                        if (y<0) {
+                            System.out.println("Неверные данные");
+                        } else {
+                            int answer;
+                            Metod t = new Metod();
+                            answer = t.pow(x,y);
+                            System.out.print("Число "+x+" в степени "+y+" равно: "+answer);
+                        }
+                        break;
+                    }
+                    case 6: {
+                        int x;
+                        System.out.print("Введите число x: ");
+                        x = scanner.nextInt();
+                        boolean answer;
+                        Metod t = new Metod();
+                        answer = t.equalNum(x);
+                        System.out.print("Все знаки числа одинаковы? "+answer);
+                        break;
+                    }
+                    case 8: {
+                        int x;
+                        System.out.print("Введите число x: ");
+                        x = scanner.nextInt();
+                        if (x <= 0) {
+                            System.out.println("Неверные данные");
+                        } else {
+                            Metod t = new Metod();
+                            t.leftTriangle(x);
+                        }
+                        break;
+                    }
+                    case 10: {
+                        Metod t = new Metod();
+                        t.guessGame();
+                        break;
+                    }
+                }
                 break;
             }
             case 4:
--- laba 1/src/Metod.java
+++ laba 1/src/Metod.java
@@ -1,38 +1,188 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Metod {
 
-    public int sumLastNums (int x){ //1.2 задание
+    // 1.2 задание
+    public int sumLastNums(int x) {
         int c = x%100;
         return c % 10 + c / 10;
     }
-    public boolean isPositive (int x){ //1.4 задание спросить на счёт проверки на целое число!!!
+
+    // 1.4 задание
+    public boolean isPositive(int x) {
         return x > 0; // заменяет конструкцию  if (x > 0) else
     }
 
-    public boolean isUpperCase (char x){ //1.6 задание
+    // 1.6 задание
+    public boolean isUpperCase(char x) {
         return 'A'<=x && x<='Z';
     }
 
-    public boolean isDivisor (int a, int b){ // 1.8 задание
-        if(a!=0 && b!=0){
+    // 1.8 задание
+    public boolean isDivisor(int a, int b) {
+        if (a!=0 && b!=0) {
             return a%b==0 || b%a==0;
-        }else if (a==0 && b==0){
+        } else if (a==0 && b==0) {
             return false;
-        }else{
+        } else {
             return true;
         }
     }
 
-    public int lastNumSum(int a, int b){ //1.10 задание
-        if (a<0){
+    // 1.10 задание
+    public int lastNumSum(int a, int b) {
+        if (a<0) {
             a=a*(-1)%10;
-        }else{
+        } else {
             a=a%10;
         }
-        if (b<0){
+        if (b<0) {
             b=b*(-1)%10;
-        }else{
+        } else {
             b=b%10;
         }
         return a+b;
+    }
+
+    // 2.2 задание
+    public double safeDiv(int x, int y) {
+        if (x==0 || y==0) {
+            return 0;
+        } else {
+            return (double) x/y;
+        }
+    }
+
+    // 2.4 задание
+    public String makeDecision(int x, int y) {
+        if (x!=y) {
+            if (x>y) {
+                return Integer.toString(x)+" > "+Integer.toString(y);
+            } else {
+                return Integer.toString(x)+" < "+Integer.toString(y);
+            }
+        } else {
+            return Integer.toString(x)+" == "+Integer.toString(y);
+        }
+    }
+
+    // 2.6 задание
+    public boolean sum3(int x, int y, int z) {
+        return (x+y==z || x+z==y || y+z==x);
+    }
+
+    // 2.8 задание
+    public String age(int x) {
+        int s = x%10;
+        if (s>4 || (x>=11 && x<=14)) {
+            return Integer.toString(x)+" лет";
+        } else if (s>=2) {
+            return Integer.toString(x)+" года";
+        } else {
+            return Integer.toString(x)+" год";
+        }
+    }
+
+    // 2.10 задание
+    public void printDays(String x) {
+        switch (x) {
+            case "понедельник": {
+                System.out.print(x + "\nвторник" + "\nсреда" + "\nчетверг" + "\nпятница" +
+                        "\nсуббота" + "\nвоскресенье");
+                break;
+            }
+            case "вторник": {
+                System.out.print(x + "\nсреда" + "\nчетверг" + "\nпятница" +
+                        "\nсуббота" + "\nвоскресенье");
+                break;
+            }
+            case "среда": {
+                System.out.print(x + "\nчетверг" + "\nпятница" +
+                        "\nсуббота" + "\nвоскресенье");
+                break;
+            }
+            case "четверг": {
+                System.out.print(x + "\nпятница" + "\nсуббота" + "\nвоскресенье");
+                break;
+            }
+            case "пятница": {
+                System.out.print(x + "\nсуббота" + "\nвоскресенье");
+                break;
+            }
+            case "суббота": {
+                System.out.print(x + "\nвоскресенье");
+                break;
+            }
+            case "воскресенье": {
+                System.out.print(x);
+                break;
+            }
+            default: System.out.println("это не день недели"); break;
+        }
+    }
+
+    // 3.2 задание
+    public String reverseListNums(int x) {
+        String s="";
+        for (int i=x;i>=0;i--) {
+            s = s + Integer.toString(i)+" ";
+        }
+        return s;
+    }
+
+    // 3.4 задание
+    public int pow(int x, int y) {
+        int p = 1;
+        for (int i=0;i<y;i++) { //учитываем 0 степень, поэтому <
+            p=p*x;
+        }
+        return p;
+    }
+
+    // 3.6 задание
+    public boolean equalNum(int x) {
+        if (x < 0) { // если пользыватель ввёл отриц. число
+            x = -x;
+        }
+        int c = x%10; // находим последнее число
+        while (x > 0) {
+            if (x % 10 != c) {
+                return false;
+            }
+            x = x/10;
+        }
+        return true;
+    }
+
+    // 3.8 задание
+    public void leftTriangle(int x) {
+        for (int i = 1;i <= x;i++) {
+            int y = 1;
+            while (y <= i) {
+                System.out.print("*");
+                y++;
+            }
+            System.out.println(); // переход на следующую строчку
+        }
+    }
+
+    // 3.10 задание
+    public void guessGame() {
+        Random random = new Random();
+        int comp = random.nextInt(0,10);
+        int k = 1; // счётчик попыток
+        int user; // под число пользывателя
+        System.out.print("Введите число от 0 до 9: ");
+        Scanner scanner = new Scanner(System.in);
+        user = scanner.nextInt();
+
+        while (comp != user) {
+            k++;
+            System.out.print("Вы не угадали, введите число от 0 до 9: ");
+            user = scanner.nextInt();
+        }
+        System.out.println("Вы угадали!\n" +
+                "Вы отгадали число за "+k+" колличества попыток.");
     }
 }
